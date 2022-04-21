43d62d12a3e085d7ae11da78fd8e1538c4090d91
FROSTYLAN <castillo089frosty@gmail.com>
2022-04-20 20:31:58 -0500

    get users with express

--- expressBasic/app.js
+++ expressBasic/app.js
@@ -1,8 +1,28 @@
 const express = require('express');
 
+// Init express app
 const app = express();
 
-const PORT = 4000;
+// Dummy data
+const users = [
+    {id: 1, name:'Joe'},
+    {id:2, name:'John'},
+    {id:3, name:'Sara'}
+]
+const posts = [
+    {id: 1, title: 'Post 1'},
+    {id: 2, title: 'Post 2'},
+    {id: 3, title: 'Post 3'}
+]
+
+//Endpoint
+app.get('/users', (req, res) => {
+    res.status(200).json({
+        users
+    });
+});
+
+const PORT = 3000;
 
 app.listen(PORT,() => {
     console.log(`Express app running on port: ${PORT}`);
